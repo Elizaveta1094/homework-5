562936cbadc63fcd941bb654d9f3eaa9c86b6f25
Elizaveta1094 <treninskaya.elizaveta@yandex.ru>
2022-12-02 14:10:28 +0300

    homework 5

---
+++ 5.1.38 task/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 38: Задайте массив вещественных(тип int) чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int [] FillArray(int size)
+{
+
+int[] arr = new int[size];
+for(int i = 0; i < arr.Length; i++)
+{
+arr[i] = new Random().Next(1,100);
+}
+return arr;
+}
+
+Console.Write("Введите длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+Console.WriteLine("Ваш массив:"+ string.Join(" ",array));
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i]>max)
+    {
+      max = array[i];
+    }
+     if(array[i]<min)
+     {
+       min = array[i];
+     }
+   
+}
+
+Console.Write($"Разницы между максимальным и минимальным числом равна { max-min } ");
---
+++ 5.2.36 task/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях(индексах).
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+int[] FillArray(int size)
+{
+
+int[] arr = new int[size];
+for(int i = 0; i < arr.Length; i++)
+{
+arr[i] = new Random().Next(-10,100);
+}
+return arr;
+}
+
+Console.Write("Введите длину массива: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+int sum = 0;
+Console.WriteLine("Ваш массив:"+ string.Join(" ",array));
+for(int i = 0; i< array.Length; i+=2)
+{
+    sum = array[i]+sum;
+   
+}
+Console.Write($"Сумма элементов равна {sum}");
